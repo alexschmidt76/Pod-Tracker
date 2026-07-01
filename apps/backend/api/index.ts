88b82a88facd8437476c1b6257ce6ac3ab78aec9
alex <alex.schmidt767@gmail.com>
2026-07-01 17:22:19 -0500

    vercel serverless fn for landing page

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,10 +1,10 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     if (req.method !== 'GET') {
-        res.status(405).json({ error: 'Method not allowed.' });
+        res.status(405).json({ error: 'Method not allowed' });
         return;
     }
 
-    res.status(200).json({ message: 'Welcome to the Pod Tracker backend api.' });
+    res.status(200).json({ message: 'Welcome to the Pod Tracker backend API.' });
 }
